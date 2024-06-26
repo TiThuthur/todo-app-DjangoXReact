3ff0e77e45f93730bbe269c8c4daa871d23c589f
TiThuthur <thurthur.petit.py@gmail.com>
2024-06-26 13:43:43 +0200

    frontend terminé

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
+import Modal from "./components/Modal";
 import "./App.css";
+import axios from "axios";
 
 const todoItems = [
   {
@@ -34,8 +36,36 @@ class App extends Component {
     this.state = {
       viewCompleted: false,
       todoList: todoItems,
+      modal: false,
+      activeItem: {
+        title: "",
+        description: "",
+        completed: false,
+      },
     };
   }
+
+  toggle = () => {
+    this.setState({ modal: !this.state.modal });
+  };
+
+  handleSubmit = (item) => {
+    this.toggle();
+
+    alert("save" + JSON.stringify(item));
+  };
+
+  handleDelete = (item) => {
+    alert("delete" + JSON.stringify(item));
+  };
+
+  createItem = () => {
+    const item = { title: "", description: "", completed: false };
+    this.setState({ activeItem: item, modal: !this.state.modal });
+  };
+  editItem = (item) => {
+    this.setState({ activeItem: item, modal: !this.state.modal });
+  };
 
   displayCompleted = (status) => {
     if (status) {
@@ -81,8 +111,19 @@ class App extends Component {
           {item.title}
         </span>
         <span>
-          <button className="btn btn-secondary mr-2"> Edit </button>
-          <button className="btn btn-danger">Delete</button>
+          <button
+            className="btn btn-secondary mr-2"
+            onClick={() => this.editItem(item)}
+          >
+            {" "}
+            Edit{" "}
+          </button>
+          <button
+            className="btn btn-danger"
+            onClick={() => this.handleDelete(item)}
+          >
+            Delete
+          </button>
         </span>
       </li>
     ));
@@ -96,7 +137,9 @@ class App extends Component {
           <div className="col-md-6 col-sm-10 mx-auto p-0">
             <div className="card p-3">
               <div className="mb-4">
-                <button className="btn btn-primary">Add task</button>
+                <button className="btn btn-primary" onClick={this.createItem}>
+                  Add task
+                </button>
               </div>
               {this.renderTabList()}
               <ul className="list-group list-group-flush border-top-0">
@@ -105,6 +148,13 @@ class App extends Component {
             </div>
           </div>
         </div>
+        {this.state.modal ? (
+          <Modal
+            activeItem={this.state.activeItem}
+            toggle={this.toggle}
+            onSave={this.handleSubmit}
+          />
+        ) : null}
       </main>
     );
   }
--- frontend/src/components/Modal.js
+++ frontend/src/components/Modal.js
@@ -31,6 +31,48 @@ export default class CustomModal extends Component {
 
   render() {
     const { toggle, onSave } = this.props;
-    return();
+    return(
+      <Modal isOpen={true} toggle={toggle}>
+        <ModalHeader toggle={toggle}>Todo Item</ModalHeader>
+        <ModalBody>
+          <Form>
+            <FormGroup>
+              <Label for="todo-title">Title</Label>
+              <Input type="text" id="todo-title" name="title" value={this.state.activeItem.title} onChange={this.handleChange} placeholder="Enter Todo title"/>
+            </FormGroup>
+            <FormGroup>
+              <Label for="todo-description" > Description</Label>
+              <Input
+              type="text"
+              id="todo-description"
+              name="description"
+              value={this.state.activeItem.description}
+              onChange={this.handleChange}
+              placeholder="Enter Todo description"
+            />
+            </FormGroup>
+            <FormGroup check>
+              <Label check>
+                <Input
+                  type="checkbox"
+                  name="completed"
+                  checked={this.state.activeItem.completed}
+                  onChange={this.handleChange}
+                />
+                Completed
+              </Label>
+            </FormGroup>
+          </Form>
+        </ModalBody>
+        <ModalFooter>
+          <Button
+            color="success"
+            onClick={() => onSave(this.state.activeItem)}
+          >
+            Save
+          </Button>
+        </ModalFooter>
+      </Modal> 
+    );
   }
 }
